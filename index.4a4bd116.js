87a3c2e6c0f8b41f1b4c3ee588eca7e1fb37d7a1
VladimirKartsev <122696359+VladimirKartsev@users.noreply.github.com>
2023-06-04 10:14:37 +0000

    Deploying to gh-pages from @ ViktoriaReminna/blended-parcel@4d9c4daa5a8cfc9716254bdb3803e9c2fa44c389 🚀

---
+++ index.4a4bd116.js
@@ -0,0 +1,5 @@
+!function(){let e=document.querySelector(".eventThumb");document.addEventListener("keydown",function(n){let t=`<ul class="eventList">
+  <li class="eventCode">${n.code}</li>
+  <li class="eventKey">${n.key}</li>
+  </ul>`;e.insertAdjacentHTML("beforeend",t)})}();
+//# sourceMappingURL=index.4a4bd116.js.map
---
+++ index.ced33ca2.js
@@ -0,0 +1,5 @@
+const e=document.querySelector(".eventThumb");document.addEventListener("keydown",function(n){let t=`<ul class="eventList">
+  <li class="eventCode">${n.code}</li>
+  <li class="eventKey">${n.key}</li>
+  </ul>`;e.insertAdjacentHTML("beforeend",t)});
+//# sourceMappingURL=index.ced33ca2.js.map
