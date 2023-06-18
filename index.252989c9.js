37f8954f07a9eb04ef9e489447d145d5a937e583
AlexBabanskiy <124041034+AlexBabanskiy@users.noreply.github.com>
2023-06-18 10:17:31 +0000

    Deploying to gh-pages from @ ViktoriaReminna/blended-parcel@3fe2759651769f772dc53e8d9a4d8002ca70586b 🚀

--- index.252989c9.js
+++
@@ -1,5 +0,0 @@
-const e=document.querySelector(".eventThumb");document.addEventListener("keydown",function(t){let c=`<ul class="eventList">
-  <li class="eventCode">${t.code}</li>
-  <li class="eventKey">${t.key}</li>
-  </ul>`;e.insertAdjacentHTML("beforeend",c)});const t=document.querySelector("#checkbox"),c=document.querySelector("body"),s=localStorage.getItem("theme");"dark"===s?(c.classList.add("dark"),checkbox.checked=!0):c.classList.add("light"),t.addEventListener("click",function(){!0===checkbox.checked&&(c.classList.add("dark"),c.classList.remove("light"),localStorage.setItem("theme","dark")),!1===checkbox.checked&&(c.classList.add("light"),c.classList.remove("dark"),localStorage.setItem("theme","light"))});
-//# sourceMappingURL=index.252989c9.js.map
--- index.36cdae24.js
+++
@@ -1,5 +0,0 @@
-!function(){let e=document.querySelector(".eventThumb");document.addEventListener("keydown",function(t){let c=`<ul class="eventList">
-  <li class="eventCode">${t.code}</li>
-  <li class="eventKey">${t.key}</li>
-  </ul>`;e.insertAdjacentHTML("beforeend",c)});let t=document.querySelector("#checkbox"),c=document.querySelector("body"),l=localStorage.getItem("theme");"dark"===l?(c.classList.add("dark"),checkbox.checked=!0):c.classList.add("light"),t.addEventListener("click",function(){!0===checkbox.checked&&(c.classList.add("dark"),c.classList.remove("light"),localStorage.setItem("theme","dark")),!1===checkbox.checked&&(c.classList.add("light"),c.classList.remove("dark"),localStorage.setItem("theme","light"))})}();
-//# sourceMappingURL=index.36cdae24.js.map
