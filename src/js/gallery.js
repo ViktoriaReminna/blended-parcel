a08cd95b3416066246e4e5998ac0b0f8ac964336
ok-qa <olechka.kr@gmail.com>
2023-07-02 02:15:41 -0700

    Created search by Query

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,9 +3,23 @@ import { Unsplash } from './unsplash';
 import { createGalleryCard } from './create-gallery-card';
 
 const galleryEl = document.querySelector('.gallery');
+const searchForm = document.querySelector('.js-search-form');
 
 const service = new Unsplash();
 service.getPopularImage(1).then(response => {
   const markup = createGalleryCard(response.results);
   galleryEl.insertAdjacentHTML('afterbegin', markup);
+});
+
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const searchValue = event.target.elements.query.value;
+
+  service.query = searchValue;
+
+  service.getImageByQuery(1).then(response => {
+    const markup = createGalleryCard(response.results);
+    galleryEl.innerHTML = '';
+    galleryEl.insertAdjacentHTML('afterbegin', markup);
+  });
 });
--- src/js/unsplash.js
+++ src/js/unsplash.js
@@ -1,6 +1,7 @@
 export class Unsplash {
   #Base_url = 'https://api.unsplash.com/search/photos';
   #Api_key = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
+  #query = '';
 
   async getPopularImage(page) {
     return await fetch(
@@ -10,6 +11,18 @@ export class Unsplash {
     )
       .then(response => response.json())
       .catch(error => console.log(error.message));
+  }
+  async getImageByQuery(page) {
+    return await fetch(
+      `${this.#Base_url}?client_id=${this.#Api_key}&query=${
+        this.#query
+      }&page=${page}&per_page=9&orientation=portrait`
+    )
+      .then(response => response.json())
+      .catch(error => console.log(error.message));
+  }
+  set query(newQuery) {
+    this.#query = newQuery;
   }
 }
 // https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
